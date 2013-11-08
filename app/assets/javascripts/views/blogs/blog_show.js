892fd8f0a9d4c949a3e772d4dbdc34a5e113c350
AJ Gregory <bertie458@gmail.com>
2013-11-08 13:20:03 -0800

    posts are now added via modal, button toolbar added

--- app/assets/javascripts/views/blogs/blog_show.js
+++ app/assets/javascripts/views/blogs/blog_show.js
@@ -2,90 +2,77 @@ JournalApp.Views.BlogShow = Backbone.View.extend({
 
   template: JST['blogs/show'],
 
-	events: {
-		'dblclick .name':'editName',
-		'dblclick .description':'editDescription',
-		'blur .nameEditBox':'updateName',
-		'blur .descriptionEditBox':'updateDescription',
-		'click .addPostBtn':'addPost',
+  events: {
+    'dblclick .name':'editName',
+    'dblclick .description':'editDescription',
+    'blur .nameEditBox':'updateName',
+    'blur .descriptionEditBox':'updateDescription',
+    'click .addPostBtn':'addPost',
     'click .followBtn':'followBlog',
-    'click .unfollowBtn':'unfollowBlog'
-	},
+    'click .unfollowBtn':'unfollowBlog',
+    'click .delete':'deleteBlog',
+    'click .postText':'postText',
+    'hidden.bs.modal':'render'
+  },
 
-	render: function() {
+  render: function() {
     var that = this;
 
-		var renderedContent = this.template({
-			blog: this.model,
-			blogPosts: this.model.blogPosts
-		});
+    var renderedContent = this.template({
+      blog: this.model,
+      blogPosts: this.model.blogPosts
+    });
 
     this.$el.html(renderedContent);
 
     this.model.blogPosts.each(function(post) {
       that.$el.children('.posts').append(that.renderPost(post));
     });
-		return this;
-	},
+    return this;
+  },
 
   renderPost: function(post) {
     postView = new JournalApp.Views.PostShow({ model: post });
     return postView.render().$el;
   },
 
-	editName: function() {
-		var boxTemp = JST['blogs/nameEditBox'];
-		box = boxTemp({
-			attr: this.model.get('name')
-		});
-
-		$('.name').html(box);
-	},
-
-	editDescription: function() {
-		var boxTemp = JST['blogs/descriptionEditBox'];
-		box = boxTemp({
-			attr: this.model.get('description')
-		});
-
-		$('.description').html(box);
-	},
-
-	updateName: function(event) {
-		var newName = $(event.currentTarget).val();
-    this.model.set({
-			"name":newName
-		});
-
-		this.model.save();
-    $('.name').html(newName);
-	},
-
-	updateDescription: function(event) {
-		var newDescription = $(event.currentTarget).val();
-    this.model.set({
-			"description":newDescription
-		});
-
-		this.model.save();
-    $('.description').html(newDescription);
-	},
-
-	addPost: function() {
-		var newPost = new JournalApp.Models.Post();
-		var postTitle = $('.newPostTitle').val();
-		var postBody = $('.newPostBody').val();
-
-		newPost.set({
-      "title": postTitle,
-      "body": postBody
+  editName: function() {
+    var boxTemp = JST['blogs/nameEditBox'];
+    box = boxTemp({
+      attr: this.model.get('name')
     });
 
-		this.model.blogPosts.add(newPost);
-		this.model.save();
+    $('.name').html(box);
+  },
 
-    $('.posts').append('<li>' + newPost.escape('title') + '</li>');
-	},
+  editDescription: function() {
+    var boxTemp = JST['blogs/descriptionEditBox'];
+    box = boxTemp({
+      attr: this.model.get('description')
+    });
+
+    $('.description').html(box);
+  },
+
+  updateName: function(event) {
+    var newName = $(event.currentTarget).val();
+    this.model.set({
+      "name":newName
+    });
+
+    this.model.save();
+    $('.name').html(newName);
+  },
+
+  updateDescription: function(event) {
+    var newDescription = $(event.currentTarget).val();
+    this.model.set({
+      "description":newDescription
+    });
+
+    this.model.save();
+    $('.description').html(newDescription);
+  },
 
   followBlog: function() {
     var that = this;
@@ -118,7 +105,6 @@ JournalApp.Views.BlogShow = Backbone.View.extend({
 
         for(var i = 0; i < that.model.followers().length; i++){
           if (that.model.followers()[i] === current_user.id){
-            debugger
             that.model.followers().splice(i, 1);
           }
         };
@@ -128,5 +114,33 @@ JournalApp.Views.BlogShow = Backbone.View.extend({
         $('.followBtn').html('Follow');
       }
     });
+  },
+
+  deleteBlog: function() {
+    this.model.destroy();
+    $('.content').empty();
+    Backbone.history.navigate("/", { trigger: true })
+  },
+
+  addPost: function() {
+    var that = this;
+
+    var newPost = new JournalApp.Models.Post();
+    var postTitle = $('#newPostTitle').val();
+    var postBody = $('#newPostBody').val();
+
+    newPost.set({
+      "title": postTitle,
+      "body": postBody
+    });
+
+     this.model.blogPosts.add(newPost);
+    this.model.save();
+  },
+
+  postText: function() {
+    var postFormTemp = JST['blogs/text_post'];
+
+    $('#post-text-modal .modal-body').html(postFormTemp());
   }
 });
--- app/assets/javascripts/views/blogs/blogs_index.js
+++ app/assets/javascripts/views/blogs/blogs_index.js
@@ -2,29 +2,19 @@ JournalApp.Views.BlogsIndex = Backbone.View.extend({
   template: JST['blogs/index'],
 
   initialize: function() {
-  	this.listenTo(this.collection, 'add', this.render);
-		this.listenTo(this.collection, 'change:title', this.render);
-		this.listenTo(this.collection, 'remove', this.render);
-		this.listenTo(this.collection, 'reset', this.render);
+    this.listenTo(this.collection, 'add', this.render);
+    this.listenTo(this.collection, 'change:title', this.render);
+    this.listenTo(this.collection, 'remove', this.render);
+    this.listenTo(this.collection, 'reset', this.render);
   },
 
-	events: {
-    "click .delete":"deleteBlog",
-	},
+  render: function() {
+    renderedContent = this.template({
+      blogs: this.collection
+    });
 
-	render: function() {
-		renderedContent = this.template({
-			blogs: this.collection
-		});
-
-		this.$el.html(renderedContent);
-		return this;
-	},
-
-	deleteBlog: function(event) {
-		var blogID = $(event.currentTarget).attr('data-id');
-		var model = this.collection.get(blogID)
-		model.destroy();
-	}
+    this.$el.html(renderedContent);
+    return this;
+  }
 
 });
