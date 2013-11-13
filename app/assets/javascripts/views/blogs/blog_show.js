bed4a8a091103829a5bb3d29448d549f5c1ffb67
AJ Gregory <bertie458@gmail.com>
2013-11-13 14:51:30 -0800

    error messages appear when creating an invalid post

--- app/assets/javascripts/views/blogs/blog_show.js
+++ app/assets/javascripts/views/blogs/blog_show.js
@@ -139,7 +139,11 @@ JournalApp.Views.BlogShow = Backbone.View.extend({
     })
 
      this.model.blogPosts.create(newPost, {
+       success: function() {
+         that.$el.find('#post-text-modal').modal('hide');
+       },
        error: function() {
+         that.$el.find('#post-text-modal .errors').fadeIn();
          that.model.blogPosts.remove(newPost);
        }
      });
@@ -148,14 +152,13 @@ JournalApp.Views.BlogShow = Backbone.View.extend({
   postText: function() {
     var postFormTemp = JST['blogs/text_post'];
 
-    $('#post-text-modal .modal-body').html(postFormTemp());
+    $('#post-text-modal .modal-body').append(postFormTemp());
   },
 
   choosePhoto: function() {
    var that = this;
    filepicker.setKey('AynRnvKa9T6WSrxRv1ExZz');
 
-   $('#post-photo-modal .modal-body').addClass('dropPane');
    $('.dropPane').html("Drag a photo here!")
 
     filepicker.makeDropPane($('.dropPane'), {
@@ -191,13 +194,18 @@ JournalApp.Views.BlogShow = Backbone.View.extend({
     var newPost = new JournalApp.Models.Post();
 
     newPost.set({
-      'photo_url': this.photoUrl
+      'photo_url': this.photoUrl,
+      'blog_id': this.model.get('id')
     });
 
     this.model.blogPosts.create(newPost, {
+      success: function() {
+        that.$el.find('#post-photo-modal').modal('hide');
+      },
       error: function() {
-         that.model.blogPosts.remove(newPost);
-       }
+        that.$el.find('#post-photo-modal .errors').fadeIn();
+        that.model.blogPosts.remove(newPost);
+      }
     });
   }
 
