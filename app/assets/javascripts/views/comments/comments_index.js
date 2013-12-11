d1c28feb718426df7a2033d9f3d6e47109dec921
AJ Gregory <bertie458@gmail.com>
2013-12-11 13:50:44 -0700

    comment destroy bugfix

--- app/assets/javascripts/views/comments/comments_index.js
+++ app/assets/javascripts/views/comments/comments_index.js
@@ -1,26 +1,23 @@
 JournalApp.Views.CommentsIndex = Backbone.View.extend({
   template: JST['comments/index'],
 
-  initialize: function() {
-		this.listenTo(this.collection, 'remove', this.render);
+  events: {
+    "click .deleteComment":"deleteComment"
   },
 
-	events: {
-    "click .delete":"deleteComment"
-	},
+  render: function() {
+    renderedContent = this.template({
+      comments: this.collection
+    });
 
-	render: function() {
-		renderedContent = this.template({
-			comments: this.collection
-		});
+    this.$el.html(renderedContent);
+    return this;
+  },
 
-		this.$el.html(renderedContent);
-		return this;
-	},
-
-	deleteComment: function(event) {
-		var commentID = $(event.currentTarget).attr('data-id');
-		var model = this.collection.get(commentID)
-	  model.destroy();
-	}
+  deleteComment: function(event) {
+    var commentID = $(event.currentTarget).attr('data-id');
+    var comment = this.collection.get(commentID);
+    $("p[data-id =" + commentID + "]").remove()
+    comment.destroy();
+  }
 });
